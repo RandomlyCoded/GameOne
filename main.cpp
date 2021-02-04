3358749deb8773a592d9dd1dbd7a6818ed7ac4bb
Mathias Hasselmann <mathias@taschenorakel.de>
2021-02-04 09:59:46 +0100

    Programm-Code in statische Bibliothek verschoben
    Damit wir Tests schreiben können

--- main.cpp
+++ main.cpp
@@ -1,48 +1,6 @@
-#include "backend.h"
-#include "inventorymodel.h"
-#include "levelmodel.h"
-#include "mapmodel.h"
-
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QQmlContext>
-
-namespace GameOne {
-
-class Application : public QGuiApplication
-{
-    Q_OBJECT
-
-public:
-    using QGuiApplication::QGuiApplication;
-
-    int run();
-};
-
-int Application::run()
-{
-    QQmlApplicationEngine qml;
-
-    qmlRegisterType<InventoryModel>("GameOne", 1, 0, "InventoryModel");
-    qmlRegisterType<LevelModel>("GameOne", 1, 0, "LevelModel");
-    qmlRegisterType<MapModel>("GameOne", 1, 0, "MapModel");
-
-    const auto backend = new Backend{this};
-    backend->load(arguments().count() > 1 ? arguments().at(1) : Backend::levelFileName(1));
-    qml.rootContext()->setContextProperty("backend", backend);
-
-    qml.load(QUrl{"qrc:/GameOne/qml/main.qml"});
-    if (qml.rootObjects().isEmpty())
-        return EXIT_FAILURE;
-
-    return exec();
-}
-
-} // namespace GameOne
+#include "application.h"
 
 int main(int argc, char *argv[])
 {
     return GameOne::Application{argc, argv}.run();
 }
-
-#include "main.moc"
